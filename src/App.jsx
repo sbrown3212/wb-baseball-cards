8b235d8201fd974bd540c2a02fbe278c02ab0b7e
Stephen Brown <stephen.jr36@gmail.com>
2024-08-19 14:55:01 -0600

    completed step 5: Add the event handler

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,56 @@
 import playerData from "./playerData";
+import { useState } from "react";
 
 function BaseballCard({name, team, position, stats, imgUrl, cardId}) {
-  // return (
-  //   <div className="card">
-  //     <h2>{name}</h2>
-  //     <img src={imgUrl} alt="" />
-  //   </div>
-  // );
-  const statsDisplay = [];
+  let [showPicture, setShowPicture] = useState(true);
 
-  for (const key in stats) {
-    const element = <p>{key}: {stats[key]}</p>;
-    statsDisplay.push(element);
+  const toggleCard = () => setShowPicture(!showPicture);
 
-    // Lab instructions solution:
+  if (showPicture) {
+    return (
+        <div className="card" onClick={toggleCard}>
+          <h2>{name}</h2>
+          <img src={imgUrl} alt="" />
+        </div>
+      );
+  } else {
+    const statsDisplay = [];
+    for (const key in stats) {
+      const element = <p>{key}: {stats[key]}</p>;
+      statsDisplay.push(element);
+    }
+
+    // // Lab instructions solution:
     // const statsDisplay = Object.entries(props.stats).map(([statName, statValue]) => (
     //   <p key={statName}>
     //     {statName}: {statValue}
     //   </p>
     // ));
-  }
-
-  return (
-    <div className="card">
-      <h2>{name}</h2>
-      <p>{team}</p>
-      <p>{position}</p>
-      {statsDisplay}
-    </div>
-  )
+    
+    return (
+      <div className="card" onClick={toggleCard}>
+        <h2>{name}</h2>
+        <p>Team: {team}</p>
+        <p>Position: {position}</p>
+        {statsDisplay}
+      </div>
+    )
+  } 
 }
 
 const cards = playerData.map((player) => {
-  return BaseballCard(player);
+  // console.log(player)
+  return <BaseballCard 
+    name={player.name}
+    team={player.team}
+    position={player.position}
+    stats={player.stats}
+    imgUrl={player.imgUrl}
+    cardId={player.cardId}
+  />;
 });
 
 function App() {
-  // console.log(cards)
   return <>{cards}</>;
 }
 
