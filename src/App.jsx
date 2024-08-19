74f3168d1330ccb5657f46d5e952e558ab563c3a
Stephen Brown <stephen.jr36@gmail.com>
2024-08-19 13:56:54 -0600

    completed step 3: create 'back' view

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import playerData from "./playerData";
 
 function BaseballCard({name, team, position, stats, imgUrl, cardId}) {
+  // return (
+  //   <div className="card">
+  //     <h2>{name}</h2>
+  //     <img src={imgUrl} alt="" />
+  //   </div>
+  // );
+  const statsDisplay = [];
+
+  for (const key in stats) {
+    const element = <p>{key}: {stats[key]}</p>;
+    statsDisplay.push(element);
+
+    // Lab instructions solution:
+    // const statsDisplay = Object.entries(props.stats).map(([statName, statValue]) => (
+    //   <p key={statName}>
+    //     {statName}: {statValue}
+    //   </p>
+    // ));
+  }
+
   return (
     <div className="card">
       <h2>{name}</h2>
-      <img src={imgUrl} alt="" />
+      <p>{team}</p>
+      <p>{position}</p>
+      {statsDisplay}
     </div>
-  );
+  )
 }
 
 const cards = playerData.map((player) => {
@@ -14,7 +36,7 @@ const cards = playerData.map((player) => {
 });
 
 function App() {
-  console.log(cards)
+  // console.log(cards)
   return <>{cards}</>;
 }
 
